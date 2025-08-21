e71f1caa5285af07bba2f2cdcea3654b4d37744d
LuyandaMbalenhle <144934497+LuyandaMbalenhle@users.noreply.github.com>
2025-08-21 02:07:07 +0200

    Admin domain class

--- backend/src/main/java/za/ca/cput/factory/RouteFactory.java
+++ backend/src/main/java/za/ca/cput/factory/RouteFactory.java
@@ -1,7 +1,5 @@
 package za.ca.cput.factory;
 
-import za.ca.cput.domain.Route;
-
 public class RouteFactory {
     public static Route createRoute(String id, String name) {
         return new Route(id, name);
