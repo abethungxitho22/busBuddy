3164e844614e9d9bb4cfd34402289d72bbf72190
Abethu Ngxitho <221297820@mycput.ac.za>
2025-08-24 21:00:07 +0200

    updated shuttle domain

--- backend/src/main/java/za/ca/cput/domain/Shuttle.java
+++ backend/src/main/java/za/ca/cput/domain/Shuttle.java
@@ -12,7 +12,10 @@ public class Shuttle {
     private Long shuttleId;
     private int capacity;
     private String licensePlate;
-    //private ShuttleOperator shuttleOperator;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn( name = "operator_id", nullable = false)
+    private ShuttleOperator operator;
 
     public Shuttle(){
 
@@ -22,6 +25,8 @@ public class Shuttle {
         this.shuttleId = builder.shuttleId;
         this.capacity = builder.capacity;
         this.licensePlate = builder.licensePlate;
+        this.operator = builder.operator;
+
     }
 
     public Long getShuttleId() {
@@ -36,12 +41,17 @@ public class Shuttle {
         return licensePlate;
     }
 
+    public ShuttleOperator getOperator() {
+        return operator;
+    }
+
     @Override
     public String toString() {
         return "Shuttle{" +
                 "shuttleId=" + shuttleId +
                 ", capacity=" + capacity +
                 ", licensePlate='" + licensePlate + '\'' +
+                ", operator=" + (operator != null ? operator.getOperatorId() : "null") +
                 '}';
     }
 
@@ -50,6 +60,7 @@ public class Shuttle {
         private Long shuttleId;
         private int capacity;
         private String licensePlate;
+        private ShuttleOperator operator;
 
         public Builder setShuttleId(Long shuttleId) {
             this.shuttleId = shuttleId;
@@ -66,10 +77,17 @@ public class Shuttle {
             return this;
         }
 
+
+        public Builder setOperator(ShuttleOperator operator) {
+            this.operator = operator;
+            return this;
+        }
+
         public Builder copy(Shuttle shuttle) {
             this.shuttleId = shuttle.shuttleId;
             this.capacity = shuttle.capacity;
             this.licensePlate = shuttle.licensePlate;
+            this.operator = shuttle.operator;
             return this;
         }
 
