936c541f7cc32aeaa9376d1515f33dbf6f07bf89
Abethu Ngxitho <221297820@mycput.ac.za>
2025-08-27 15:22:37 +0200

    updated

---
+++ backend/src/main/java/za/ca/cput/service/ScheduleService.java
@@ -0,0 +1,45 @@
+package za.ca.cput.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import za.ca.cput.domain.Schedule;
+import za.ca.cput.repository.ScheduleRepository;
+
+import java.util.List;
+
+public class ScheduleService implements IScheduleService{
+
+    private final ScheduleRepository repository;
+
+    @Autowired
+    public ScheduleService(ScheduleRepository repository){
+        this.repository = repository;
+    }
+
+    @Override
+    public Schedule create(Schedule schedule) {
+        return repository.save(schedule);
+    }
+
+    @Override
+    public Schedule read(Long scheduleId) {
+        return this.repository.findById(scheduleId).orElse(null);
+    }
+
+    @Override
+
+    public Schedule update(Schedule schedule) {
+        return repository.save(schedule);
+
+
+    }
+   @Override
+   public void delete(Long scheduleId) {
+       this.repository.deleteById(scheduleId);
+
+}
+
+   @Override
+   public List<Schedule> getAll() {
+        return repository.findAll();
+    }
+}
