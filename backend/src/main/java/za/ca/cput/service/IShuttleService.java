0e5c232f749fcd46b08eaf39845dbd7984857ee9
Abethu Ngxitho <221297820@mycput.ac.za>
2025-08-29 18:04:41 +0200

    updated

--- backend/src/main/java/za/ca/cput/service/IShuttleService.java
+++ backend/src/main/java/za/ca/cput/service/IShuttleService.java
@@ -1,9 +1,14 @@
 package za.ca.cput.service;
 
 import za.ca.cput.domain.Shuttle;
+import za.ca.cput.domain.ShuttleOperator;
 
 import java.util.List;
 
 public interface IShuttleService extends IService<Shuttle, Long>  {
     List<Shuttle> getAll();
+    List<Shuttle> getByShuttleOperator(ShuttleOperator operator);
+    List<Shuttle> searchBylicensePlate(String keyword);
+    List<Shuttle> filterByCapacity(int minCapacity, int maxCapacity);
 }
+//need to commit this changes
